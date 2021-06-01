9ae18d222fe3e8390cb51a82451710daf41dfc3b
Marco Orazio Re <marcooraziore@gmail.com>
2021-06-01 16:30:42 +0200

    Base scaffolding
    -  Created all Controllers/Models/Migrations/Factories/Seeders
    - Created views/routes structure

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'locations' => 'LocationController',
+    'employees' => 'EmployeeController',
+    'tasks'     => 'TaskController',
+]);
