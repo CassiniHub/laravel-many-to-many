3d70e4e19932e1e64217fadbd364553511b7c613
Marco Orazio Re <marcooraziore@gmail.com>
2021-06-01 17:52:51 +0200

    Printed data in employees.index
    - All created data are printed in employees.index view

--- resources/views/pages/employees/index.blade.php
+++ resources/views/pages/employees/index.blade.php
@@ -2,8 +2,28 @@
 
 @section('content')
     <h1>
-        Hello World
+        Employees
     </h1>
 
-    
+    <ul>
+        @foreach ($employees as $employee)
+            <li>
+                <h2>
+                    {{ $employee -> firstname }} {{ $employee -> lastname }}
+                </h2>
+
+                <h3>
+                    Location: {{ $employee -> location -> state }}, {{ $employee -> location -> address }}
+                </h3>
+
+                <ul>
+                    @foreach ($employee -> tasks as $task)
+                        <li>
+                            {{ $task -> name }}
+                        </li>
+                    @endforeach
+                </ul>
+            </li>
+        @endforeach
+    </ul>
 @endsection
